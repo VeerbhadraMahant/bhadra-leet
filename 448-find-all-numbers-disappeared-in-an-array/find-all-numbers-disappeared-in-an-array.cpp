8b241e2fc0aa6d5582e91f70de30e9cc353a01bc
Veerbhadra Shantvirappa Mahant <veerbhadramahant59@gmail.com>
2026-09-18 21:16:59 +0530

    Time: 7 ms (47.34%) | Memory: 50.1 MB (99.99%) - LeetSync

--- 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.cpp
+++ 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.cpp
@@ -1,15 +1,48 @@
+#pragma GCC optimize("Ofast")
+
+#include <bits/stdc++.h>
+using namespace std;
+
+static constexpr size_t max_align = alignof(max_align_t);
+alignas(max_align) static unsigned char BUFFER[64 * 1024 * 1024];
+static size_t pos = 0;
+
+void *operator new(const size_t size) {
+    const size_t padding = (max_align - (pos % max_align)) % max_align;
+    pos += padding + size;
+    return static_cast<void *>(&BUFFER[pos - size]);
+}
+
+void *operator new[](const size_t size) { return operator new(size); }
+void operator delete(void *) noexcept {}
+void operator delete[](void *) noexcept {}
+void operator delete(void *, size_t) noexcept {}
+void operator delete[](void *, size_t) noexcept {}
+
 class Solution {
 public:
+    Solution() {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+        cout.tie(nullptr);
+    }
     vector<int> findDisappearedNumbers(vector<int>& nums) {
-        int len = nums.size();
-        for(int i=0; i<len; i++) {
-            int m = abs(nums[i])-1;
-            nums[m] = nums[m]>0 ? -nums[m] : nums[m];
+        const int n_size = nums.size();
+
+        for (int i = n_size - 1; i >= 0; --i) {
+            const int cur = i + 1;
+            while (nums[i] != cur && nums[nums[i] - 1] != nums[i]) {
+                swap(nums[nums[i] - 1], nums[i]);
+            }
         }
-        vector<int> res;
-        for(int i = 0; i<len; i++) {
-            if(nums[i] > 0) res.push_back(i+1);
+
+        int i = 0;
+        for (int j = 0; j < n_size; ++j) {
+            if (nums[j] != j + 1) nums[i++] = j + 1;
         }
-        return res;
+
+        nums.resize(i);
+
+        return nums;
     }
 };
